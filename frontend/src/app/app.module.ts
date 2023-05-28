92df81e5dc9fbe20e853a0203cc2532b16c38258
mmihaylov-aux <mmihaylov00@gmail.com>
2023-05-28 15:28:48 +0300

    Avatar component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
@@ -45,9 +45,10 @@ import { SideNavComponent } from './components/side-nav/side-nav.component';
       heroClipboard,
       heroUser,
       heroUserGroup,
-      heroBriefcase,
+      heroBriefcase
     }),
     MatSidenavModule,
+    NgOptimizedImage
   ],
   exports: [CommonModule],
   providers: [],
--- frontend/src/app/components/avatar/avatar.component.ts
+++ frontend/src/app/components/avatar/avatar.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-avatar',
+  selector: 'app-avatar[username]',
   templateUrl: './avatar.component.html',
   styleUrls: ['./avatar.component.scss'],
 })
 export class AvatarComponent implements OnInit {
+  @Input() url?: string;
+  @Input() username: string;
   constructor() {}
 
   ngOnInit(): void {}
