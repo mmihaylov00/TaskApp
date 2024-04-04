253c91e027499b6eccee3b03f5df6c7a57975684
Miroslav Mihaylov <mmihaylov00@gmail.com>
2024-04-04 21:49:10 +0300

    Improved initial migration

--- backend/migrations/1712255542-init.migration.sql
+++ backend/migrations/1712255542-init.migration.sql
@@ -1,8 +1,10 @@
-CREATE TYPE "enum_Users_role" AS ENUM ('DEVELOPER', 'PROJECT_MANAGER', 'ADMIN');
-CREATE TYPE "enum_Users_status" AS ENUM ('INVITED', 'ACTIVE', 'DISABLED');
-CREATE TYPE "enum_Tasks_priority" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+CREATE SCHEMA IF NOT EXISTS taskapp;
 
-CREATE TABLE "Users"
+CREATE TYPE "taskapp"."enum_Users_role" AS ENUM ('DEVELOPER', 'PROJECT_MANAGER', 'ADMIN');
+CREATE TYPE "taskapp"."enum_Users_status" AS ENUM ('INVITED', 'ACTIVE', 'DISABLED');
+CREATE TYPE "taskapp"."enum_Tasks_priority" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+CREATE TABLE "taskapp"."Users"
 (
     id               UUID                     NOT NULL PRIMARY KEY,
     email            VARCHAR(255) UNIQUE,
@@ -18,7 +20,7 @@ CREATE TABLE "Users"
     "deletedAt"      TIMESTAMP WITH TIME ZONE
 );
 
-CREATE TABLE "Projects"
+CREATE TABLE "taskapp"."Projects"
 (
     id          UUID                     NOT NULL PRIMARY KEY,
     name        VARCHAR(255),
@@ -29,89 +31,90 @@ CREATE TABLE "Projects"
     "deletedAt" TIMESTAMP WITH TIME ZONE
 );
 
-CREATE TABLE "UserProjects"
+CREATE TABLE "taskapp"."UserProjects"
 (
-    "userId"    UUID                     NOT NULL REFERENCES "Users" ON UPDATE CASCADE ON DELETE CASCADE,
-    "projectId" UUID                     NOT NULL REFERENCES "Projects" ON UPDATE CASCADE ON DELETE CASCADE,
+    "userId"    UUID                     NOT NULL REFERENCES "taskapp"."Users" ON UPDATE CASCADE ON DELETE CASCADE,
+    "projectId" UUID                     NOT NULL REFERENCES "taskapp"."Projects" ON UPDATE CASCADE ON DELETE CASCADE,
     "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL,
     "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL,
     PRIMARY KEY ("userId", "projectId")
 );
 
-CREATE TABLE "Boards"
+CREATE TABLE "taskapp"."Boards"
 (
     id            UUID                     NOT NULL PRIMARY KEY,
     name          VARCHAR(255),
     color         VARCHAR(255),
     archived      BOOLEAN,
-    "projectId"   UUID REFERENCES "Projects" ON UPDATE CASCADE ON DELETE CASCADE,
+    "projectId"   UUID REFERENCES "taskapp"."Projects" ON UPDATE CASCADE ON DELETE CASCADE,
     "stagesOrder" JSONB DEFAULT '[]'::JSONB,
     "createdAt"   TIMESTAMP WITH TIME ZONE NOT NULL,
     "updatedAt"   TIMESTAMP WITH TIME ZONE NOT NULL,
     "deletedAt"   TIMESTAMP WITH TIME ZONE
 );
 
-CREATE TABLE "Stages"
+CREATE TABLE "taskapp"."Stages"
 (
     id           UUID                     NOT NULL PRIMARY KEY,
     name         VARCHAR(255),
     color        VARCHAR(255),
-    "boardId"    UUID REFERENCES "Boards" ON UPDATE CASCADE ON DELETE CASCADE,
+    "boardId"    UUID REFERENCES "taskapp"."Boards" ON UPDATE CASCADE ON DELETE CASCADE,
     "tasksOrder" JSONB DEFAULT '[]'::JSONB,
     "createdAt"  TIMESTAMP WITH TIME ZONE NOT NULL,
     "updatedAt"  TIMESTAMP WITH TIME ZONE NOT NULL,
     "deletedAt"  TIMESTAMP WITH TIME ZONE,
-    board_id     UUID                     REFERENCES "Boards" ON UPDATE CASCADE ON DELETE SET NULL
+    board_id     UUID                     REFERENCES "taskapp"."Boards" ON UPDATE CASCADE ON DELETE SET NULL
 );
 
-CREATE TABLE "Tasks"
+CREATE TABLE "taskapp"."Tasks"
 (
     id          UUID NOT NULL PRIMARY KEY,
     name        VARCHAR(255),
     description JSONB,
     priority    "enum_Tasks_priority",
-    "stageId"   UUID REFERENCES "Stages" ON UPDATE CASCADE ON DELETE CASCADE,
-    "projectId" UUID REFERENCES "Projects" ON UPDATE CASCADE ON DELETE CASCADE,
-    author      UUID REFERENCES "Users" ON UPDATE CASCADE ON DELETE CASCADE,
-    assignee    UUID REFERENCES "Users" ON UPDATE CASCADE ON DELETE SET NULL,
-    "updatedBy" UUID REFERENCES "Users" ON UPDATE CASCADE ON DELETE SET NULL,
+    "stageId"   UUID REFERENCES "taskapp"."Stages" ON UPDATE CASCADE ON DELETE CASCADE,
+    "projectId" UUID REFERENCES "taskapp"."Projects" ON UPDATE CASCADE ON DELETE CASCADE,
+    author      UUID REFERENCES "taskapp"."Users" ON UPDATE CASCADE ON DELETE CASCADE,
+    assignee    UUID REFERENCES "taskapp"."Users" ON UPDATE CASCADE ON DELETE SET NULL,
+    "updatedBy" UUID REFERENCES "taskapp"."Users" ON UPDATE CASCADE ON DELETE SET NULL,
     deadline    TIMESTAMP WITH TIME ZONE,
     "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     "updatedAt" TIMESTAMP WITH TIME ZONE,
     deleted     BOOLEAN,
     completed   BOOLEAN,
     thumbnail   VARCHAR(255),
-    uploader    UUID REFERENCES "Users" ON UPDATE CASCADE ON DELETE SET NULL
+    uploader    UUID REFERENCES "taskapp"."Users" ON UPDATE CASCADE ON DELETE SET NULL
 );
 
-CREATE TABLE "Attachments"
+CREATE TABLE "taskapp"."Attachments"
 (
     id        UUID NOT NULL
         PRIMARY KEY,
     name      VARCHAR(255),
     mime      VARCHAR(255),
     extension VARCHAR(255),
-    uploader  UUID REFERENCES "Users" ON UPDATE CASCADE,
-    "taskId"  UUID REFERENCES "Tasks" ON UPDATE CASCADE ON DELETE CASCADE
+    uploader  UUID REFERENCES "taskapp"."Users" ON UPDATE CASCADE,
+    "taskId"  UUID REFERENCES "taskapp"."Tasks" ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE "Notifications"
+CREATE TABLE "taskapp"."Notifications"
 (
     id          UUID NOT NULL PRIMARY KEY,
     message     VARCHAR(255),
     link        VARCHAR(255),
     read        BOOLEAN,
     deleted     BOOLEAN,
-    "userId"    UUID REFERENCES "Users" ON UPDATE CASCADE ON DELETE CASCADE,
+    "userId"    UUID REFERENCES "taskapp"."Users" ON UPDATE CASCADE ON DELETE CASCADE,
     "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
 
-CREATE INDEX users_email ON "Users" (email);
-CREATE INDEX user_project ON "UserProjects" ("userId", "projectId");
-CREATE INDEX boards_archived ON "Boards" (archived);
-CREATE INDEX stages_name ON "Stages" (name);
-CREATE INDEX closed ON "Tasks" (deleted, completed);
-CREATE INDEX tasks_deadline ON "Tasks" (deadline);
-CREATE INDEX notifications_read ON "Notifications" (read);
-CREATE INDEX notifications_deleted ON "Notifications" (deleted);
-CREATE INDEX notifications_created_at ON "Notifications" ("createdAt");
+CREATE INDEX users_email ON "taskapp"."Users" (email);
+CREATE INDEX user_project ON "taskapp"."UserProjects" ("userId", "projectId");
+CREATE INDEX boards_archived ON "taskapp"."Boards" (archived);
+CREATE INDEX stages_name ON "taskapp"."Stages" (name);
+CREATE INDEX closed ON "taskapp"."Tasks" (deleted, completed);
+CREATE INDEX tasks_deadline ON "taskapp"."Tasks" (deadline);
+CREATE INDEX notifications_read ON "taskapp"."Notifications" (read);
+CREATE INDEX notifications_deleted ON "taskapp"."Notifications" (deleted);
+CREATE INDEX notifications_created_at ON "taskapp"."Notifications" ("createdAt");
+
